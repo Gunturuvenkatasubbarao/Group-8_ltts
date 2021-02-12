95496ffdd0071f375d902c2f17ecf884a7e9e45f
Gunturuvenkatasubbarao <gunturu.venkatasubbarao@ltts.com>
2021-02-12 17:24:49 +0530

    Update Goldloan.c

--- Implementation/Src/Goldloan.c
+++ Implementation/Src/Goldloan.c
@@ -9,6 +9,7 @@ void Goldloan(){
     double pfee=0.05*amount;
     double interest=0.07*amount;
     double total=amount+pfee+interest;
+    //comment
     printf("Sanctioned amount %d:",amount);
     printf("Total repayment %lf:",total);
 }
