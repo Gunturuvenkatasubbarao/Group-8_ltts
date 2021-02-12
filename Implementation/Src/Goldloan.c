cd3455275d3020702f95e606df915aae62de1b0a
Gunturuvenkatasubbarao <gunturu.venkatasubbarao@ltts.com>
2021-02-12 17:21:23 +0530

    commit1

---
+++ Implementation/Src/Goldloan.c
@@ -0,0 +1,14 @@
+void Goldloan(){
+    int grams;
+    printf("Enter Grams:\n");
+    scanf("%d",&grams);
+    int months;
+    printf("Enter no of months:\n");
+    scanf("%d",&months);
+    int amount=grams*2500;
+    double pfee=0.05*amount;
+    double interest=0.07*amount;
+    double total=amount+pfee+interest;
+    printf("Sanctioned amount %d:",amount);
+    printf("Total repayment %lf:",total);
+}
