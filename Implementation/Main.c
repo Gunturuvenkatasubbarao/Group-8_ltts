5142d5356f12e547b8c22bdebf16c322bf740bc0
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-02-13 10:10:39 +0530

    Update Main.c

--- Implementation/Main.c
+++ Implementation/Main.c
@@ -3,42 +3,19 @@
 
 #include "Goldloan.h"
 #include "Carloan.h"
+#include "verifyAge.h"
 
-void do_Goldloan();
-void do_Carloan();
-
-
-void opt()
+void Goldloan();
+void Carloan();
+void verifyage();
+int main()
 {
-	printf("\n             Welcome to Loan calculator \n\n");
-
-	printf("Enter 1 for Goldloan \n");
-	printf("Enter 2 for Carloan \n");
-	
+	 char name[20];
+	   printf("enter name:");
+	   scanf("%s", name);
+	   verifyAge();
+	    return 0;
 }
 
-int main(void)
-{
-	int X = 1;
-	char input;
-	char respo;
-
-	opt();
-	scanf("%c", &input);
-	switch(input)
-	{ 
-		case '1': do_Goldloan();
-				break; 
-		case '2': do_carloan();
-				break;
-		
-		default : 
-			printf("\n**********You have entered unavailable option");
-    		printf("***********\n");
-    		printf("\n*****Please Enter any one of below available ");
-    		printf("options****\n");
-			opr();
-
-	}
 
 	
