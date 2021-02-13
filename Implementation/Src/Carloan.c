b6b405986f0096af3a907494e2b9112f56030eba
karthikkumargundu <78849193+karthikkumargundu@users.noreply.github.com>
2021-02-13 11:15:54 +0530

    Update Carloan.c

--- Implementation/Src/Carloan.c
+++ Implementation/Src/Carloan.c
@@ -1,35 +1,13 @@
-<<<<<<< HEAD
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-#include<stdlib.h>
-#include <ctype.h>
-
-int main()
+void carloan()
 {
-	    int age,ch,months,months1,amount,amt,scamt,m,m1,pan,itr;
-	    char name[20];
-	         int grams,years;
-	    double pfee,inte,total,emi,prfee,inter,ctotal;
-	   printf("enter name:");
-	   scanf("%s", name);
-	   printf("enter age \n");
-	   scanf("%d", &age);
-      if(age>=18 && age<=60){
-        	 printf("ENTER YOUR NAME \n");
-    	        scanf("%d",&pan);
-    	        printf("ENTER ITR \n");
-=======
-void CarLoan(){
-    printf("ENTER YOUR NAME \n");
+printf("enter pan number \n");
     	        scanf("%s",&pan);
-    	        printf("ENTER IT RETURNS \n");
->>>>>>> 7d1afb4c0e18bf4609ca4d03c633c6462cb92457
+    	        printf("enter ITR \n");
     	        scanf("%d",&itr);
-    	        //if(itr <100000 )
+    	        if(itr <100000 )
     	        {
     	        	printf("YOU ARE NOT ELIGIBLE FOR THE LOAN ");
-    	    //
+    	        	break;
     	        }
     	        if(itr >100000 && itr<500000){
     	            scamt = 500000;
@@ -40,7 +18,7 @@ void CarLoan(){
     	        }
     	       
     	        
-    	        printf("ENTER THE TIME IN YEARS");
+    	        printf("enter the time in years");
     	        scanf("%d",&m);
     	        m1=m*12;
     	        prfee = (0.5/100)*scamt;
@@ -48,10 +26,6 @@ void CarLoan(){
                 ctotal = scamt+ prfee+inter;
                 
                 
-                printf("SANCTINED AMOUNT IS %d \n",scamt);
-        	    printf("TOTAL REPAYMENT %lf \n" ,ctotal);
-<<<<<<< HEAD
-	  }
- }
-=======
->>>>>>> 7d1afb4c0e18bf4609ca4d03c633c6462cb92457
+                printf("Sanctioned loan amount is %d \n",scamt);
+        	    printf("total repayment %lf \n" ,ctotal);
+}
