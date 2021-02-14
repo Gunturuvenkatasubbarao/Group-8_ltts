ff269b4764299d33a1a90d22b1550cf85f0050e4
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-02-14 17:52:53 +0530

    Update Main.c

--- Implementation/Main.c
+++ Implementation/Main.c
@@ -3,19 +3,51 @@
 
 #include "Goldloan.h"
 #include "Carloan.h"
-#include "verifyAge.h"
 
 void Goldloan();
 void Carloan();
-void verifyage();
 int main()
 {
 	 char name[20];
+	 int age;
 	   printf("enter name:");
 	   scanf("%s", name);
-	   verifyAge();
+	   printf("Enter age:\n");
+	   scanf("%d",&age);
+	   
+	   int count=1;
+	   while(count)
+	   {
+	   	if(age<18)
+	   	{
+	   		printf("Not eligible: ");
+	   		scanf("%d",&age);
+		}
+		else
+		{
+			count=0;
+		}
+	   }
+	   int value;
+	   printf("1. Gold Loan\n");
+	   printf("2.Car Loan\n");
+	   printf("choose : \n");
+	   scanf("%d",&value);
+	   switch(value){
+            case 1:
+            GoldLoan();
+            break;
+            case 2:
+            Carloan();
+            break;
+            
+            default:
+            	printf("Invalid");
+            
+        }
+	   
+	   
 	    return 0;
 }
-
 
 	
